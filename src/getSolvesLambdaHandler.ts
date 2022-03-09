23b3511d8a423a2d957418ad90d911501928f0c8
spencerkasper <spener.kasper@gmail.com>
2022-03-08 21:13:47 -0600

    * Ugh the lambda is returning a weird error

--- src/getSolvesLambdaHandler.ts
+++ src/getSolvesLambdaHandler.ts
@@ -5,7 +5,8 @@ export const handler = async (event: any) => {
     try {
         const userId = event.userId;
         const dynamoDbClient = new DynamoDbClient('solve_log_solves');
-        const result = await dynamoDbClient.queryTable(`SELECT * FROM "solve_log_solves" WHERE "userId" = '${userId}'`);
+        const whereClause = userId ? ` WHERE "userId" = '${userId}'` : '';
+        const result = await dynamoDbClient.queryTable(`SELECT * FROM "solve_log_solves"${whereClause}`);
         console.error(result)
         return {responseCode: 200, body: SolveLogSolvesSchema.fromSchema(result.Items)};
     } catch (e) {
