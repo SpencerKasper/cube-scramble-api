ed0e77821b7042bef7c91c007581e162a954aef3
spencerkasper <spener.kasper@gmail.com>
2022-03-08 21:45:17 -0600

    * Return solves object

--- src/getSolvesLambdaHandler.ts
+++ src/getSolvesLambdaHandler.ts
@@ -5,10 +5,12 @@ export const handler = async (event: any) => {
     try {
         const userId = event.userId;
         const dynamoDbClient = new DynamoDbClient('solve_log_solves');
+        console.error(userId);
         const whereClause = userId ? ` WHERE "userId" = '${userId}'` : '';
         const result = await dynamoDbClient.queryTable(`SELECT * FROM "solve_log_solves"${whereClause}`);
         console.error(result)
-        return {responseCode: 200, body: SolveLogSolvesSchema.fromSchema(result.Items)};
+        const solves = SolveLogSolvesSchema.fromSchema(result.Items);
+        return {responseCode: 200, body: {solves}};
     } catch (e) {
         return e;
     }
