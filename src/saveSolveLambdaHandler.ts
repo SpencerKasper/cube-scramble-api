7eebea1ed46e793a1a3ab0cadd8ad9ce046f00f6
spencerkasper <spener.kasper@gmail.com>
2022-03-10 19:54:42 -0600

    * Maybe this will fix it?

--- src/saveSolveLambdaHandler.ts
+++ src/saveSolveLambdaHandler.ts
@@ -4,9 +4,9 @@ import {SolveLogSolvesSchema} from "./dynamo_db/schemas/solveLogSolvesSchema";
 export const handler = async (event: any) => {
     try {
         console.error(event);
-        const body = event.body;
+        const solve = event.body;
         const dynamoDbClient = new DynamoDbClient('solve_log_solves');
-        const userIdSolve = {solve: body.solve};
+        const userIdSolve = {solve};
         await dynamoDbClient.writeMany([SolveLogSolvesSchema.toSchema(userIdSolve)])
         return {responseCode: 200, body: userIdSolve, headers: {
                 "Access-Control-Allow-Headers": "Content-Type",
