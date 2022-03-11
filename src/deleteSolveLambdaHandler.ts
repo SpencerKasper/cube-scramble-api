d249c04e250b17e6cae7d97333b5d02011930240
spencerkasper <spener.kasper@gmail.com>
2022-03-10 20:51:04 -0600

    * Add a delete solve endpoint.

--- src/deleteSolveLambdaHandler.ts
+++ src/deleteSolveLambdaHandler.ts
@@ -4,9 +4,14 @@ import {SolveLogSolvesSchema} from "./dynamo_db/schemas/solveLogSolvesSchema";
 export const handler = async (event: any) => {
     try {
         const userId = event.userId;
+        const solveId = event.solveId;
+        if(!userId || !solveId) {
+            return {responseCode: 400, body: {message: 'You must pass both "userId" and "solveId" to delete a solve.'}}
+        }
         const dynamoDbClient = new DynamoDbClient('solve_log_solves');
         const whereClause = userId ? ` WHERE "userId" = '${userId}'` : '';
-        const result = await dynamoDbClient.queryTable(`SELECT * FROM "solve_log_solves"${whereClause}`);
+        await dynamoDbClient.queryTable(`DELETE FROM "solve_log_solves" ${whereClause} WHERE "userId" = '${userId}' AND "solveId" = '${solveId}'`);
+        const result = await dynamoDbClient.queryTable(`SELECT * FROM "solve_log_solves" WHERE "userId" = '${userId}'`);
         const solves = SolveLogSolvesSchema.fromSchema(result.Items);
         return {responseCode: 200, body: {solves}};
     } catch (e) {
