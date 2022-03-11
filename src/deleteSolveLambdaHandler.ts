bbfff0db4599a88701a935ebc6f69d46d1b61a56
spencerkasper <spener.kasper@gmail.com>
2022-03-10 21:12:17 -0600

    * Fix bugs in the where clause.

--- src/deleteSolveLambdaHandler.ts
+++ src/deleteSolveLambdaHandler.ts
@@ -5,13 +5,16 @@ export const handler = async (event: any) => {
     try {
         const userId = event.userId;
         const solveId = event.solveId;
-        if(!userId || !solveId) {
+        if (!userId || !solveId) {
             return {responseCode: 400, body: {message: 'You must pass both "userId" and "solveId" to delete a solve.'}}
         }
         const dynamoDbClient = new DynamoDbClient('solve_log_solves');
-        const whereClause = userId ? ` WHERE "userId" = '${userId}'` : '';
-        await dynamoDbClient.queryTable(`DELETE FROM "solve_log_solves" ${whereClause} WHERE "userId" = '${userId}' AND "solveId" = '${solveId}'`);
-        const result = await dynamoDbClient.queryTable(`SELECT * FROM "solve_log_solves" WHERE "userId" = '${userId}'`);
+        const deleteStatement = `DELETE FROM "solve_log_solves" WHERE "userId" = '${userId}' AND "solveId" = '${solveId}'`;
+        console.error(`Delete Statement: ${deleteStatement}`);
+        await dynamoDbClient.queryTable(deleteStatement);
+        const selectStatement = `SELECT * FROM "solve_log_solves" WHERE "userId" = '${userId}'`;
+        console.error(`Select Statement: ${selectStatement}`);
+        const result = await dynamoDbClient.queryTable(selectStatement);
         const solves = SolveLogSolvesSchema.fromSchema(result.Items);
         return {responseCode: 200, body: {solves}};
     } catch (e) {
