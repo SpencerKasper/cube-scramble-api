0ff900462e1980b752bf3fcf02b851b018b835ed
spencerkasper <spener.kasper@gmail.com>
2022-03-11 16:13:18 -0600

    * Add scramble length as a query param

--- src/getScrambleLambdaHandler.ts
+++ src/getScrambleLambdaHandler.ts
@@ -1,9 +1,11 @@
 import {Scramble} from './scramble/Scramble';
 
+const DEFAULT_SCRAMBLE_LENGTH = 30;
+
 export const handler = async (event: any) => {
-    const SCRAMBLE_LENGTH = 20;
+    const scrambleLength = event.scrambleLength = DEFAULT_SCRAMBLE_LENGTH;
     try {
-        const scramble = new Scramble(SCRAMBLE_LENGTH).toString();
+        const scramble = new Scramble(scrambleLength).toString();
         return {responseCode: 200, body: {scramble}};
     } catch (e) {
         return e;
