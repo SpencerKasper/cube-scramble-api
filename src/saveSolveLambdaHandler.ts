10fa79da75b30f22eac257a777d7d0a070774020
spencerkasper <spener.kasper@gmail.com>
2022-03-15 14:48:05 -0500

    * Modify save solve so that it returns all the solves.

--- src/saveSolveLambdaHandler.ts
+++ src/saveSolveLambdaHandler.ts
@@ -1,18 +1,14 @@
 import {DynamoDbClient} from "./dynamo_db/dynamoDbClient";
 import {SolveLogSolvesSchema} from "./dynamo_db/schemas/solveLogSolvesSchema";
+import * as getSolvesLambdaHandler from './getSolvesLambdaHandler';
 
 export const handler = async (event: any) => {
     try {
-        console.error(event);
         const solve = event.body;
         const dynamoDbClient = new DynamoDbClient('solve_log_solves');
         const userIdSolve = {solve};
-        await dynamoDbClient.writeMany([SolveLogSolvesSchema.toSchema(userIdSolve)])
-        return {responseCode: 200, body: userIdSolve, headers: {
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-            }};
+        await dynamoDbClient.writeMany([SolveLogSolvesSchema.toSchema(userIdSolve)]);
+        return getSolvesLambdaHandler.handler({userId: solve.userId});
     } catch (e) {
         return e;
     }
