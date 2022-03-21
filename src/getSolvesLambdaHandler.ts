5fdb9ca79e987fd92a966da1ecc4c1abd79df766
spencerkasper <spener.kasper@gmail.com>
2022-03-21 16:06:27 -0500

    * Add parens to sql statement

--- src/getSolvesLambdaHandler.ts
+++ src/getSolvesLambdaHandler.ts
@@ -4,16 +4,19 @@ import {DEFAULT_SESSION_ID, SolveLogSolvesSchema} from "./dynamo_db/schemas/solv
 export const handler = async (event: any) => {
     try {
         if (!event.userId) {
-            return {responseCode: 400, body: {solves: []}};
+            return {responseCode: 400, body: {solves: []}, message: 'Must have userId in query string'};
+        }
+        if (!event.sessionId) {
+            return {responseCode: 400, body: {solves: []}, message: 'Must have sessionId in query string'};
         }
         const userId = decodeURIComponent(event.userId);
         const dynamoDbClient = new DynamoDbClient('solve_log_solves');
         const whereClause = ` WHERE "userId" = '${userId}'`;
-        const whereClauseWithSessionId = event.sessionId ? `${whereClause} AND "sessionId" = '${event.sessionId}'` : whereClause;
-        const whereClauseWithEmptySessionIds = event.sessionId && event.sessionId === DEFAULT_SESSION_ID ? `${whereClauseWithSessionId} OR "sessionId" IS MISSING` : whereClauseWithSessionId;
-        const sqlStatement = `SELECT * FROM "solve_log_solves"${whereClauseWithEmptySessionIds}`;
-        const result = await dynamoDbClient.queryTable(sqlStatement);
+        const whereClauseWithSessionId = event.sessionId ? `${whereClause} AND ("sessionId" = '${event.sessionId}'` : whereClause;
+        const whereClauseWithEmptySessionIds = event.sessionId && event.sessionId === DEFAULT_SESSION_ID ? `${whereClauseWithSessionId} OR "sessionId" IS MISSING)` : `${whereClauseWithSessionId}`;
+        const sqlStatement = event.sessionId && event.sessionId === DEFAULT_SESSION_ID && !whereClauseWithEmptySessionIds.endsWith(')') ? `SELECT * FROM "solve_log_solves"${whereClauseWithEmptySessionIds})` : `SELECT * FROM "solve_log_solves"${whereClauseWithEmptySessionIds}`;
         console.error(`GetSolvesLambdaHandler - SQL Statement - ${sqlStatement}`);
+        const result = await dynamoDbClient.queryTable(sqlStatement);
         const solves = SolveLogSolvesSchema.fromSchema(result.Items);
         return {responseCode: 200, body: {solves}};
     } catch (e) {
